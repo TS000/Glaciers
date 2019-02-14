67328980e0a912a8ca90c53dad31717cc356be29
TS000 <tylersnell@me.com>
2019-02-14 14:35:54 -0700

    Pyramid live

--- glaciers.js
+++ glaciers.js
@@ -26,7 +26,7 @@ const sketch = ({ context }) => {
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
+  camera.position.set(10, 2, -4);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -34,22 +34,31 @@ const sketch = ({ context }) => {
 
   // Setup your scene
   const scene = new THREE.Scene();
-
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true
+  scene.fog = new THREE.FogExp2( 0xCCCFFF, 0.007 );
+    const geometry = new THREE.CylinderGeometry( 0, 5.5, 5.5, 4, false )
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      vertexColors: 0xffffff
     })
-  );
-  scene.add(mesh);
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+  // Add Wires
+  const wireframe = new THREE.WireframeGeometry(geometry)
+  const lineMaterial = new THREE.LineBasicMaterial({
+    color: 0xf8f8ff,
+    transparent: true,
+    opacity: .3,
+    depthTest: true,
+  });
+  const line = new THREE.Line( wireframe, lineMaterial );
+  scene.add(line);
 
   // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
+  scene.add(new THREE.AmbientLight('#ffffff'));
 
   // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
+  const light = new THREE.PointLight('lightblue', 1, 15.5);
   light.position.set(2, 2, -4).multiplyScalar(1.5);
   scene.add(light);
 
@@ -64,7 +73,8 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render ({ time }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
+      cube.rotation.y = time * (10 * Math.PI / 180);
+      line.rotation.y = time * (10 * Math.PI / 180);
       controls.update();
       renderer.render(scene, camera);
     },
